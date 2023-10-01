5679297d9a8fb1db8e364fbe0481a4fe2ff69b7d
ChaiyapanJack <70262687+ChaiyapanJack@users.noreply.github.com>
2023-10-01 07:24:09 +0000

    app_LogisticReg_penguin_65130701702.py

--- app_LogisticReg_penguin_65130701702.py
+++ app_LogisticReg_penguin_65130701702.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 import pickle
 
 
